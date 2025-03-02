bfb4fbb08d32440003bff7ab07fbb8e86a32ed25
Jonasz Lazar <104980334+jonasz-lazar-pwr@users.noreply.github.com>
2025-03-02 18:43:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+
+function skillsMember() {
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Node.js',
+    'MongoDB',
+    'Express',
+  ];
+
+  return skills;
+}
